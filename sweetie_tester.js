166dc106674f18f447dddb84ce0d2ebf980a555d
tastulae <tastulae@mail.usf.edu>
2015-01-05 22:42:32 +0000

    added counters

--- sweetie_tester.js
+++ sweetie_tester.js
@@ -1,13 +1,22 @@
 var fs = require('fs');
 console.log("Running Sweetietester 0.0.2");
+var totalNumberOfTests = 0;
+var totalNumberOfTestsInsufficientInfo = 0;
+var totalNumberPassed = 0;
+var totalNumberFailed = 0;
 
 function assert(message, booleanCondition) {
     if(message==undefined || booleanCondition==undefined) console.log("ERROR - incorrect usage. expected assert(message:String, testCondition:Boolean)");
+    totalNumberOfTestsInsufficientInfo++;
     else if(booleanCondition) console.log("PASSED - "+message);
+    totalNumberPassed++;
     else console.log("FAILED - "+message);
+    totalNumberFailed++;
+    totalNumberOfTests++;
 }
 
 var testFileContent = fs.readFileSync('./spec.js').toString();
 eval(testFileContent);
 
+console.log("Number of tests passed"+totalNumberPassed+", number of tests failed"+totalNumberFailed+", number of tests with insufficient information"+totalNumberOfTestsInsufficientInfo+", total number of tests"+totalNumberOfTests)
 console.log("Test complete");
